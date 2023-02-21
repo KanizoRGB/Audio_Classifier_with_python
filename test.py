3a31c262ffeca442c77ec3dc28faf487f2ca6282
Leo Maina <leonardhophni@gmail.com>
2023-02-21 20:51:29 +0300

    Initial commit

--- test.py
+++ test.py
@@ -3,14 +3,21 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import librosa
 import numpy as np
+import pandas as pd
+from playsound import playsound
 from tensorflow.keras.models import load_model
+
+####DATABASE
+data = pd.read_csv("Audio_database_dataset.csv", header=None)
+df = pd.DataFrame(data)
+
 
 ####### ALL CONSTANTS #####
 fs = 44100
 seconds = 2
-filename1 = "beep.wav"
-filename2 = "sample_with_noise.wav"
-filename3 = "negative.wav"
+filename1 = df.iat[0,0] #Beep.wav file in database
+filename2 = df.iat[1,0] #Sample wav file with beep sound and noise in background
+filename3 = df.iat[3,0] #Just a noise wav file
 
 class_names = ["Tone NOT Detected", "Tone Detected"]
 
@@ -20,7 +27,7 @@ model = load_model("saved_model/WWD.h5")
 i = 0
 while True:
     
-    key = input("Enter number 1, 2 or 3: ")
+    key = input("Choose audio file in database by pressing 1, 2 or 3: ")
     print(key)
     
     try:
@@ -28,11 +35,13 @@ while True:
             audio, sample_rate = librosa.load(filename1)
             mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
             mfcc_processed = np.mean(mfcc.T, axis=0)
+            playsound(filename1)
 
             prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
             if prediction[:, 1] > 0.99:
                 print(f"Tone Detected for ({i})")
                 print("Confidence:", prediction[:, 1])
+                print("Rotate servo motor by 10 degrees")
                 i += 1
     
             else:
@@ -40,14 +49,17 @@ while True:
                 print("Confidence:", prediction[:, 0])
 
         if (key == '2'):
+            
             audio, sample_rate = librosa.load(filename2)
             mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
             mfcc_processed = np.mean(mfcc.T, axis=0)
+            playsound(filename2)
 
             prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
             if prediction[:, 1] > 0.99:
                 print(f"Tone Detected for ({i})")
                 print("Confidence:", prediction[:, 1])
+                print("Rotate servo motor by 10 degrees")
                 i += 1
     
             else:
@@ -58,11 +70,13 @@ while True:
             audio, sample_rate = librosa.load(filename3)
             mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
             mfcc_processed = np.mean(mfcc.T, axis=0)
+            playsound(filename3)
 
             prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
             if prediction[:, 1] > 0.99:
                 print(f"Tone Detected for ({i})")
                 print("Confidence:", prediction[:, 1])
+                print("Rotate servo motor by 10 degrees")
                 i += 1
     
             else:
